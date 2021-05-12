74de61f1243cb83b9e6d782b0b282dfb8ad0e0ef
Phillip Jensen <phillip@golemgrid.com>
2021-05-12 14:26:25 +0200

    Update ALLOWED_HOST

--- stats-monitor/core/settings.py
+++ stats-monitor/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['api.golemstats.com', 'localhost']
+ALLOWED_HOSTS = ['status.golemstats.com', ]
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
